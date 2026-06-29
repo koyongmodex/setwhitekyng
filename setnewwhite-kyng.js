3f9b1ba9501dfa70f7059fe23b6657bb9a077200
koyongmodex <lontongkhoirul0@gmail.com>
2026-06-29 11:15:49 +0700

    Fix HTML escaping in setnewwhite-kyng.js

--- setnewwhite-kyng.js
+++ setnewwhite-kyng.js
@@ -1,17 +1,14 @@
-let body = `
+et body = `
 <!DOCTYPE html>
 <html>
 <head>
 <title>dadu online - Penelusuran</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 </head>
-
 <body style="background:#ffffff;margin:0;padding:0;overflow:hidden;">
 <div style="height:env(safe-area-inset-top);background:#ffffff;width:100%;position:sticky;top:0;z-index:999;"></div>
-
 <iframe src="https://karduscosmos.com/index.php"
 style="width:100vw;height:130vh;border:0;display:block;background:#ffffff;"></iframe>
-
 </body>
 </html>
 `;

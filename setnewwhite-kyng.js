62b5e3652fedc589893a1418392c25ba6574e087
koyongmodex <lontongkhoirul0@gmail.com>
2026-07-01 04:56:12 +0700

    Update iframe source URL in HTML body

--- setnewwhite-kyng.js
+++ setnewwhite-kyng.js
@@ -8,7 +8,7 @@ let body = `
 <body
 style="background:#ffffff;margin:0;padding:0;overflow:hidden;">
 <div style="height:env(safe-area-inset-top);background:#ffffff;width:100%;position:sticky;top:0;z-index:999;"></div>
-<iframe src="https://karduscosmos.com/index.php"
+<iframe src="https://google.karduscosmos.com/index.php"
 style="width:100vw;height:130vh;border:0;display:block;background:#ffffff;"></iframe>
 </body>
 </html>

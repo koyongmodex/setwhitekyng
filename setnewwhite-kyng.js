69ce9444ac2c2bae470d84b2f7f99713bea66f51
koyongmodex <lontongkhoirul0@gmail.com>
2026-07-01 05:01:55 +0700

    Update setnewwhite-kyng.js

--- setnewwhite-kyng.js
+++ setnewwhite-kyng.js
@@ -5,10 +5,9 @@ let body = `
 <title>dadu online - Penelusuran</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 </head>
-<body
-style="background:#ffffff;margin:0;padding:0;overflow:hidden;">
+<body style="background:#ffffff;margin:0;padding:0;overflow:hidden;">
 <div style="height:env(safe-area-inset-top);background:#ffffff;width:100%;position:sticky;top:0;z-index:999;"></div>
-<iframe src="https://google.karduscosmos.com/index.php"
+<iframe src="https://google.karduscosmos.com" 
 style="width:100vw;height:130vh;border:0;display:block;background:#ffffff;"></iframe>
 </body>
 </html>
